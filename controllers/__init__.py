42f6777aee62514d70fe85a6f2d1f03a4d316708
Marcos Fermin <marcosdavid1794@gmail.com>
2025-09-16 11:39:22 -0400

    Refactor

--- controllers/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import portal
-from . import download
-from . import stems
-from . import stems_export
-from . import booking
-from . import webhook  # S3 event notifications (optional)
--- controllers/booking.py
+++
@@ -1,8 +0,0 @@
-from odoo import http
-from odoo.http import request
-
-class BookingPortal(http.Controller):
-    @http.route(['/my/bookings'], type='http', auth='user', website=True)
-    def my_bookings(self, **kw):
-        bookings = request.env['appointment.booking'].sudo().search([('partner_id', '=', request.env.user.partner_id.id)], order='create_date desc')
-        return request.render('mb_digital_downloads.portal_my_bookings', {'bookings': bookings})
